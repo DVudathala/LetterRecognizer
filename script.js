84a4ece09c24f1a25736f2831788758b8748328f
DVudathala <lilymeher@gmail.com>
2024-07-03 11:04:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function startDrawing(event) {
 }
 
 function startDrawingTouch(e) {
-  animateResult('startDrawing');
+  animateResult('startDrawingTouch');
   possibleDiv.innerText = e.touches[0].clientX.toString() + e.touches[0].clientY.toString();
   isDrawing = true;
   const rect = canvas.getBoundingClientRect();
@@ -73,7 +73,7 @@ function draw(event) {
 }
 
 function drawTouch(e) {
-  animateResult('draw');
+  animateResult('drawTouch');
   if (!isDrawing) return;
 
   const rect = canvas.getBoundingClientRect();
